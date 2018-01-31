3d9195b926bb7d43cad009bb596859345dc27364
Sean McGowan <(none)>
2018-01-30 22:54:58 -0500

    Test that ntp is running and enabled

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,3 +10,10 @@ def test_ntp_is_installed(host):
     p = host.package('ntp')
 
     assert p.is_installed
+
+
+def test_ntp_is_running_and_enabled(host):
+    s = host.service('ntpd')
+
+    assert s.is_running
+    assert s.is_enabled
